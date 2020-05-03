bbfa239ed05c543076401ed11a0c6e1a10aefdbb
kulukulu94 <eexma89@nottingham.ac.uk>
2020-05-03 20:18:50 +0100

    Variables in Python

---
+++ pythonrefresh.py
@@ -0,0 +1,27 @@
+#intergers, floats
+x = 15
+
+price = 9.99
+
+discount = 0.2 
+
+result = price * (1 - discount)
+
+print(result)
+
+#strings
+
+name = "Ali"
+print(name * 2 ) #this basicly means AliAli
+
+#changing variable value
+
+a = 25 
+b = a
+print(a)
+print(b)
+
+b = 17
+
+print(a)
+print(b)
