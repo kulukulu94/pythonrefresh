48e33eeb3b75f47c616f7ef0c248733f8419f4d1
kulukulu94 <eexma89@nottingham.ac.uk>
2020-05-06 09:03:39 +0100

    age calculator app

---
+++ monthageapp.py
@@ -0,0 +1,5 @@
+user_age =int(input("Enter your age: "))
+months = user_age * 12
+days   = months * 30
+seconds= days * 60 * 60 * 24
+print(f"Your are , {user_age}, is equal to {months} months, equal to {days} days, equal to {seconds} seconds.")
