cf36e6b51a3ed50c72fec7911c40a75c37f379dd
kulukulu94 <eexma89@nottingham.ac.uk>
2020-05-08 10:52:55 +0100

    boolean, if statement, in keyword

--- pythonrefresh.py
+++ pythonrefresh.py
@@ -31,12 +31,13 @@ formatted = longer_phrase.format("Mohammed", "Wednesday")
 #print(formatted)
 
 #Getting User Input
-name = input("Enter your name?")
+#name = input("Enter your name?")
 #print("Hi",name)
 
 #Lists, tuples and sets
 l  = ["Mohd", "Mudathir", "ALi"] # can be changed, maintain order & allow duplicates
 t  = ("Mohd", "Mudathir", "ALi") # can not be changed, maintain order & allow duplicates
+my_tuple = 15, #this is a single value tuble
 s  = {"Mohd", "Mudathir", "ALi"} # can be changed, doesn't maintain order & doesn't allow duplicates
 
 l.append("mahi") # can't add to tuble
@@ -48,6 +49,54 @@ science = {"Khalid", "Omer" "Jon", "Lily", "Han"}
 dif  = art.difference(science)
 all  = art.union(science)
 both = art.intersection(science)
-print(dif)
-print(all)
-print(both)
+#print(dif)
+#print(all)
+#print(both)
+
+#booleans in python
+#print(5==5)
+#print(5 > 5)
+#print(10 != 10)
+# comparisons: ==, !=, >, <, >=, <=
+
+#is key word checks if the elements are exactly the same thing not ust they have the same elements/values
+
+#if statements 
+
+day_of_week = input("what day of the week is it today?").lower()
+
+if day_of_week == "monday":
+    print("Have a great start to your week!")
+elif day_of_week == "tuesday":
+    print("It's Tuesday")
+else:
+    print("Full speed ahead!")
+    
+# in keyword works in lists, tuples, sets and strings
+
+watched_movies = {"The Matrix", "Green Book", "Her"}    
+user_movie = input("Enter something you have watched recently: ")
+
+print( user_movie in watched_movies)
+
+#if statement with in keyword
+watched_movies = {"The Matrix", "Green Book", "Her"}    
+user_movie = input("Enter something you have watched recently: ")
+
+if user_movie in watched_movies:
+    print(f"I have watched {user_movie} too!")
+else:
+    print("I have not watched that yet!")    
+     
+####
+number = 7
+user_input = input("Enter 'y' if you would like to play: ").lower()
+
+if user_input == 'y':#it can be user in ("y", "Y")
+    user_number = int(input("Guess our number: "))
+    if user_number == number:
+        print("You guessed correctly")
+    elif abs(number - user_number) == 1:#can be (number - user_number) in (1, -1)
+        print("You were off by one.")
+    else:
+        print("sorry, it's wrong!")      
