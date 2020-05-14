e58d5eb659de5091a1ab63ee2889d7850cd0cf9b
kulukulu94 <eexma89@nottingham.ac.uk>
2020-05-14 20:58:03 +0100

    Destructing variables, Functions in python, destructing vars, func. args. & prams. , def params values, fun returning vals, lambda, dic comprehen. ,unpacking args, unpacking keywords args

--- pythonrefresh.py
+++ pythonrefresh.py
@@ -101,7 +101,7 @@ if user_input == 'y':#it can be user in ("y", "Y")
     else:
         print("sorry, it's wrong!")      
  '''
-#loops while, 
+##loops while, 
 ''' 
 
 while True:
@@ -120,7 +120,7 @@ while True:
         print("sorry, it's wrong!")      
          '''
 
-''' #loops, for
+''' ##loops, for
 friends = ["Rolf", "Jon", "Bon"]
 
 for friend in friends:
@@ -132,16 +132,141 @@ total  = sum(grades)
 amount  = len(grades)
 print(total/amount)   
  '''
-''' #list comprehension
+''' ##list comprehension
 friends = ["Rolf", "Sam", "Samantha", "sony", "Jon"]
 starts_s = [friend for friend in friends if friend.startswith("S") ]
 print(starts_s)
 
  '''
-# dictionaries python dictionaries similar to php associative arrays
+""" ## dictionaries python dictionaries similar to php associative arrays
 student_attendance = {"Jon": 90, "Elly":30, "Mark": 44}
 for student, attendance in student_attendance.items():
     print(f"{student}: {attendance}")
     
 attendance_values =student_attendance.values()
 print(sum(attendance_values) / len(attendance_values))
+ """
+""" ##estructuring variables
+x = 5, 11 #it's a tuble not nessecarly to have ()
+
+x, y = 5, 11 # better than assigning vales separately
+
+t = 5, 11
+x, y = t
+
+print(x, y)# python is smart enought to get that from t
+
+# for student, attendance in student_attendance.items(): this splitng is called destructuring
+
+head, *tail = [1, 2, 3, 4, 5] # head = 1 , *tail =the rest
+*head, tail = [1, 2, 3, 4, 5] # *head = [1, 2, 3, 4]  , tail =the last 5
+ """
+
+""" ##Functions
+def hello():
+    print(hello)
+
+hello()# calls the function
+#age calculator function
+ """
+# u can't reuse function's name inside it for variables
+
+#concept of global and local variable within the function
+
+# u cant call fuction before its defined
+
+
+""" ##Functions Arguments and parameters
+def add(x,y):
+    pass # means do nothing
+#u cant put positonal argumeent after named argument add(x=5, 6)
+#you cant pass positional arguments to a function that doesn't have parameters
+#to be cont....
+ """
+##Default parameter Values
+""" def add(x, y=7):
+    print(x + y)# y is optional one argument can be passed as y default
+     """
+#u cant have def add(x=5, y)
+
+#when function encounter return it wil terminate and ingore rest of lines
+
+##Lambda functions
+""" 
+add = lambda x, y: x+y
+print((lambda x, y: x+y)(5, 7))
+
+
+def double():
+    return x * 2
+
+sequence =[1, 3, 5, 9]
+
+doubled = [double(x) for x in sequence]
+#instead can be
+doubled =map(double, sequence)# applies each of seq. to double func.
+ """
+""" #using lambda
+sequence =[1, 3, 5, 9]
+#doubled = [(lambda x: x * 2)(x) for x in sequence]
+doubled =list(map(lambda x: x * 2, sequence))# 
+print(doubled)
+
+ """
+""" ##dictionary comprehensions
+users = [
+    (0,"Jon", "Password"),
+    (1, "Khalid", "3rrsdfdsaf"),
+    (2,"Jame", "wordx"),
+    (3, "Khan", "000f")
+]
+
+username_mapping = {user[1]: user for user in users}
+print(username_mapping)
+ """
+""" ##unpacking argument  this is awy of collecting multiple arguments into a single papameter
+def multiply(*args):# this means a set of arguments will be collected into a tuple of arguments
+    print(args)
+    for arg in args:
+        total = total * arg
+
+    return total
+
+print(multiply(1, 3, 5))
+
+#destructuring one argument into multiple parameters
+def add(x, y):
+    return x + y
+
+nums = [3, 5]
+add(*nums)#this destructs them its the opposite of the above
+nums = {"x": 15,"y": 25}
+add(**nums)#in case of a dictionary this will take the values of the dic. keys that are exact as papameters names any pass them to each parameter
+#good practise to use same params names as dic keys to simplify the code
+
+
+def apply(*args, operator):
+    if operator == "*":
+        return multiply(args)#there is a bug as args r collected as tuple better to user multiply(*args) which upack them
+    elif operator == "+":
+        return sum(args)
+    else:
+        return "No valid operator provided to apply()."
+    
+    print(1, 4, 5, 6, operator="+")
+   """  
+    
+##unpacking keywords arguments
+
+def named(**kwargs):#this function collects named arguments into a dictionary where dic. keys = names of args
+    print(kwargs)
+    
+named(name="bob", age=25)    
+
+#the opposite of above upack a dict, into named args
+def named(name, age):
+    print(name, age)
+    
+details = {"name": "Mohammed", "age": 25}
+
+named(**details)
